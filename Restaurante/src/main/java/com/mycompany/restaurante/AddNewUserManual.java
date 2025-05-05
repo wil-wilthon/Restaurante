a76a6eafefc990e1e85b89b74f996438512b4db6
wil-wilthon <wilthon300@gmail.com>
2025-05-05 19:05:25 -0300

    Update AddNewUserManual.java

--- Restaurante/src/main/java/com/mycompany/restaurante/AddNewUserManual.java
+++ Restaurante/src/main/java/com/mycompany/restaurante/AddNewUserManual.java
@@ -14,6 +14,7 @@ import java.util.List;
  * @author User
  */
 public class AddNewUserManual {
+    /*TESTE TESTE TESTE TESTE TESTE TESTE TESTE TESTE*/
     public static void main(String[] args) {
         List<Permissoes> permissoesAdminMaster = List.of(Permissoes.MASTER);
         List<Permissoes> permissoesAdmin = List.of(Permissoes.ADMINISTRADOR);
